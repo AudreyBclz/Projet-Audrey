4f316a209430804a604a3424e0e8f30e8f0e0b51
AudreyBclz <audrey.bucholz@hotmail.fr>
2020-02-27 17:51:06 +0100

    modif prono pc

---
+++ js/diapo_article.js
@@ -0,0 +1,15 @@
+var i=2;
+
+function affiche() {
+
+
+    if (i==4){
+        i=1;
+    }
+    else {document.getElementById("diapo").src="img/articles/article"+i+".jpg";
+    i=i+1;}
+
+};
+(function diapos (){
+    setInterval(affiche,7000);
+    }());
