2afd70d9b0e076b70c39aac92b255450a4010090
62014-51-12 <62014-51-12@exchange.ad.afpanet>
2020-03-02 11:18:03 +0100

    controle formulaire inscription

---
+++ js/controle_form.js
@@ -0,0 +1,27 @@
+//à faire contrôle si pseudo déjà utilisé
+
+function controle_inscription()
+{
+    var email=document.getElementById("email").value;
+    var email_conf=document.getElementById("conf_email").value;
+    var mdp=document.getElementById("MdP").value;
+    var conf_mdp=document.getElementById("conf_MdP").value;
+    var testForm = true;
+
+    if(email !== email_conf)
+    {
+        alert("Erreur lors de la confirmation du mail");
+        testForm=false;
+    }
+    if( mdp !== conf_mdp)
+    {
+        alert("Erreur lors de la confirmation du mot de passe");
+        testForm=false;
+    }
+    if (testForm)
+    {
+        alert("Inscription réussie");
+        document.getElementById("form_inscription").submit();
+    }
+}
+document.getElementById("inscription").addEventListener("click",controle_inscription,false);
